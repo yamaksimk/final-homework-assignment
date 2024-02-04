4ace9fcc74e3fe5c67d6ba01e0c0020eea80bbf9
Максим Куракин <maksimkurakin@192.168.1.9>
2024-02-04 11:17:49 +0300

    Проверка нового массива по длине

--- vs code/Program.cs
+++ vs code/Program.cs
@@ -7,3 +7,14 @@ foreach (string i in array)
     Console.Write(i + " ");
 }
 Console.WriteLine();
+
+string[] arrayNew = new string [array.Length];
+int index = 0;
+foreach (string i in array)
+{
+    if(i.Length <= 3)
+    {
+        arrayNew[index] = i;
+        index++;
+    }
+}
