f047054be0f91c238acd8d5dfb13b1864c3e46e5
Максим Куракин <maksimkurakin@192.168.1.9>
2024-02-04 11:11:29 +0300

    создание массива, вывод его на экран

--- vs code/Program.cs
+++ vs code/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+
+string[] array = {"Geek","Brains","-","это","лучшее","место","для","обучения","программированию"};
+Console.WriteLine("Исходный массив:");
+foreach (string i in array)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
